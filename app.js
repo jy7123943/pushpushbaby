60e0f9c0c7229774acd43214ddbd3a49e79bad15
Juyeon <jy7123943@gmail.com>
2021-05-22 13:19:17 +0900

    Implement postWeeklyStudyReport

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-const { Base64 } = require('js-base64');
 
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
 
-const { convertToMarkdown, formatCurrentTime } = require('./utils');
-const { createOrUpdateGitFile, getGitFile } = require('./utils/octokit');
+const { postWeeklyStudyReport } = require('./api/weekly');
 
 const {
   SLACK_ACCESS_TOKEN,
@@ -47,36 +45,14 @@ app.post('/weekly', async (req, res) => {
 slackEvents.on('app_mention', async (event) => {
   try {
     const userMessage = event.text.replace('<@U0106J68PHP>', '').trim();
-    const {
-      year,
-      month,
-      weekOfMonth,
-      dateString,
-    } = formatCurrentTime();
 
     const { user } = await slackClient.users.info({ user: event.user });
 
-    const gitConfig = {
-      path: `${year}년_${month}월/${weekOfMonth}주차_스터디.md`,
-      owner: 'fepocha',
-      repo: 'study',
-    };
-
-    const file = await getGitFile(gitConfig);
-
-    const originalContent = file ? Base64.decode(file.content) : '';
-
-    const result = await createOrUpdateGitFile({
-      ...gitConfig,
-      accept: 'application/vnd.github.v3+json',
-      message: `Add study - ${dateString}`,
-      content: Base64.encode(originalContent + convertToMarkdown(user.profile.real_name, userMessage)),
-      sha: file ? file.sha : undefined,
-      committer: {
-        name: user.profile.real_name,
-        email: user.profile.email,
-      },
-    });
+    const result = await postWeeklyStudyReport({
+      userName: user.profile.real_name,
+      userEmail: user.profile.email,
+      userMessage,
+    })
 
     const uploadedLink = result.content.html_url;
 
