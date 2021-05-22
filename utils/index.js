e5a620a02857a9f353683e888b2541f9d5d0e179
Juyeon <jy7123943@gmail.com>
2021-05-22 14:08:44 +0900

    Add getFilePathAndCommitMessage util method

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 const locale = require('date-fns/locale/ko');
 const { format, utcToZonedTime } = require('date-fns-tz');
 const getWeekOfMonth = require('date-fns/getWeekOfMonth');
+const { UPLOAD_TYPE } = require('../constants');
 
 const { toHTML } = require('slack-markdown');
 
@@ -28,7 +29,37 @@ const formatCurrentTime = () => {
   };
 };
 
+const getFilePathAndCommitMessage = (uploadType) => {
+  const {
+    year,
+    month,
+    weekOfMonth,
+    dateString,
+  } = formatCurrentTime();
+
+  switch (uploadType) {
+    case UPLOAD_TYPE.WEEKLY:
+      return {
+        path: `스터디_리포트/${year}년_${month}월/${weekOfMonth}주차_스터디.md`,
+        message: `Upload study report - ${dateString}`,
+      };
+    case UPLOAD_TYPE.PLAN:
+      return {
+        path: `스터디_계획/${year}년_${month}월_${weekOfMonth}주차~4주_계획.md`,
+        message: `Upload study plan - ${dateString}`,
+      };
+    case UPLOAD_TYPE.MEETING:
+      return {
+        path: `스터디_회고/${year}년_${month}월_${weekOfMonth}주차_스터디_회고.md`,
+        message: `Upload study meeting - ${dateString}`,
+      };
+    default:
+      throw new Error(`${JSON.stringify(uploadType)} is not a valid type`);
+  }
+};
+
 module.exports = {
   convertToMarkdown,
   formatCurrentTime,
+  getFilePathAndCommitMessage,
 };
