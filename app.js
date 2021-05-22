8a0546d5364ec64e1dc1265b9318ca657b1f2511
Juyeon <jy7123943@gmail.com>
2021-05-22 12:21:29 +0900

    Remove IIFE

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 const slackClient = new WebClient(SLACK_ACCESS_TOKEN);
 
-slackEvents.on('app_mention', (event) => {
+slackEvents.on('app_mention', async (event) => {
   const userMessage = event.text.replace('<@U0106J68PHP>', '').trim();
   const {
     year,
@@ -34,46 +34,44 @@ slackEvents.on('app_mention', (event) => {
     dateString,
   } = formatCurrentTime();
 
-  (async () => {
-    try {
-      const { user } = await slackClient.users.info({ user: event.user });
-      const committer = {
-        name: user.profile.real_name,
-        email: user.profile.email,
-      };
+  try {
+    const { user } = await slackClient.users.info({ user: event.user });
+    const committer = {
+      name: user.profile.real_name,
+      email: user.profile.email,
+    };
 
-      const gitConfig = {
-        path: `${year}년_${month}월/${weekOfMonth}주차_스터디.md`,
-        owner: 'fepocha',
-        repo: 'study',
-      }
-
-      const file = await getGitFile(gitConfig);
-
-      const originalContent = file ? Base64.decode(file.content) : '';
-
-      const result = await createOrUpdateGitFile({
-        ...gitConfig,
-        accept: 'application/vnd.github.v3+json',
-        message: `Add study - ${dateString}`,
-        content: Base64.encode(originalContent + convertToMarkdown(user.profile.real_name, userMessage)),
-        sha: file ? file.sha : undefined,
-        committer,
-      })
-
-      const uploadedLink = result.content.html_url;
-
-      await slackClient.chat.postMessage({
-        channel: event.channel,
-        text: `<@${event.user}> 업데이트에 성공했어요! :baby: :point_right: <${uploadedLink}|Link>`,
-      });
-    } catch (error) {
-      await slackClient.chat.postMessage({
-        channel: event.channel,
-        text: `<@${event.user}> 업데이트에 실패했어요 :angel: ${error.message}`,
-      });
+    const gitConfig = {
+      path: `${year}년_${month}월/${weekOfMonth}주차_스터디.md`,
+      owner: 'fepocha',
+      repo: 'study',
     }
-  })();
+
+    const file = await getGitFile(gitConfig);
+
+    const originalContent = file ? Base64.decode(file.content) : '';
+
+    const result = await createOrUpdateGitFile({
+      ...gitConfig,
+      accept: 'application/vnd.github.v3+json',
+      message: `Add study - ${dateString}`,
+      content: Base64.encode(originalContent + convertToMarkdown(user.profile.real_name, userMessage)),
+      sha: file ? file.sha : undefined,
+      committer,
+    })
+
+    const uploadedLink = result.content.html_url;
+
+    await slackClient.chat.postMessage({
+      channel: event.channel,
+      text: `<@${event.user}> 업데이트에 성공했어요! :baby: :point_right: <${uploadedLink}|Link>`,
+    });
+  } catch (error) {
+    await slackClient.chat.postMessage({
+      channel: event.channel,
+      text: `<@${event.user}> 업데이트에 실패했어요 :angel: ${error.message}`,
+    });
+  }
 });
 
 slackEvents.on('error', console.error);
